03f549564433e03fde90eba1491e90ecb024f3f5
OrkhanMustafayev <orkhan.mustafayev@ailab.az>
2025-04-01 22:41:40 +0400

    added base domain entity

---
+++ src/main/java/az/hamburg/managementsystem/domain/BaseDomain.java
@@ -0,0 +1,31 @@
+package az.hamburg.managementsystem.domain;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class BaseDomain {
+
+    private LocalDateTime created;
+    private String createdBy;
+    private LocalDateTime modified;
+    private String modifiedBy;
+
+    @PrePersist
+    protected void onCreate() {
+        this.created = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        if (this.created == null) {
+            this.created = LocalDateTime.now();
+        }
+        this.modified = LocalDateTime.now();
+    }
+
+}
